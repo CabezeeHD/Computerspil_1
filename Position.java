d54189fcb5359b35adc0283438ea140f24b45f65
Victor Isachsen <victor.isachsen@gmail.com>
2022-11-08 21:37:04 +0100

    Opgave 3 start med accessor metoder

---
+++ Position.java
@@ -0,0 +1,57 @@
+/**
+ * @author Victor Isachsen
+ * @author Mads Munk
+ */
+public class Position {
+    private City from;
+    private City to;
+    private int distance;
+    private int total;
+
+    /**
+     * Konstruktør der initialisere feltvariablerne.
+     * @param from      Start position i byen.
+     * @param to        Slut position i byen.
+     * @param distance  Den afstand der er tilbage.
+     */
+    public Position(City from, City to, int distance) {
+        this.from = from;
+        this.to = to;
+        this.distance = distance;
+        this.total = distance; //Distancen mellem to byer.
+    }
+
+    /**
+     * Metode der returnere et City objekt som er start byen for vejen.
+     * @return  Returnere et City objekt.
+     */
+    public City getFrom() {
+        return from;
+    }
+
+    /**
+     * Metode der returnere et City objekt som er slut byen for vejen.
+     * @return  Returnere et City objekt.
+     */
+    public City getTo() {
+        return to;
+    }
+
+    /**
+     * Metode der returnere den resterende afstand.
+     * @return  Returnere den afstand der er tilbage.
+     */
+    public int getDistance() {
+        return distance;
+    }
+
+    /**
+     * Metode der returnere distancen mellem to byer.
+     * @return  Returnere længden på vejen mellem to byer.
+     */
+    public int getTotal() {
+        return total;
+    }
+
+
+}
